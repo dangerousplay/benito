78c95de5b51e95120c629d15c6690fc79e2b1e2e
Dangerousplay <dangerousplay715@gmail.com>
2023-11-22 18:38:19 -0300

    [Added] Submenu and register entity initial implementation

--- apps/benito-web/src/pages/home/SideBar.tsx
+++ apps/benito-web/src/pages/home/SideBar.tsx
@@ -1,40 +1,61 @@
 import React from "react";
 import {
-    Route,
     Link, useLocation
 } from "react-router-dom";
 
 
-type Route = {
-    paths: string;
+export type Submenu = {
+    paths: string[];
+    title: string;
+}
+
+export type Menu = {
+    paths: string[];
     title: string;
     icon: React.ReactElement;
     main: () => React.ReactElement;
+    submenus: Submenu[]
 }
 
 type SideBarProps = {
-    routes: Route[];
+    menus: Menu[];
     routePrefix: string;
 }
 
-export default function Sidebar({ routes, routePrefix }: Readonly<SideBarProps>) {
+export default function Sidebar({ menus, routePrefix }: Readonly<SideBarProps>) {
     const location = useLocation();
 
     return (
-        <ul className={"max-w-min list-none space-y-2"}>
-            {routes.map(r => {
+        <ul className={"list-none space-y-2"}>
+            {menus.map(r => {
                 const path = r.paths[0];
                 const routePath = routePrefix + path
-                const active = routePath === location.pathname;
+                let active = routePath === location.pathname || location.pathname.startsWith(routePath);
+
                 const selectedClass = active ? "bg-gray-200" : "hover:bg-gray-100";
 
                 return (
                     <li key={path} className={``}>
                         <Link to={routePath}>
-                            <div className={`flex flex-row gap-x-4 p-5 rounded-lg ${selectedClass}`}>
+                            <div className={`flex flex-row gap-x-4 p-5 mr-6 rounded-lg ${selectedClass}`}>
                                 {r.icon}
                                 <p>{r.title}</p>
                             </div>
+                            {r?.submenus && <div className={"ml-4 mt-3"}>
+                                {r?.submenus?.map(s => {
+                                    const subroutePath = `${routePath}/${s.paths[0]}`;
+                                    const active = subroutePath === location.pathname;
+                                    const selectedClass = active ? "bg-gray-100" : "hover:bg-gray-100";
+
+                                    return (
+                                        <div className={`rounded-lg p-2 ${selectedClass}`} key={subroutePath}>
+                                            <Link to={subroutePath}>
+                                                <p>{s.title}</p>
+                                            </Link>
+                                        </div>
+                                    )
+                                })}
+                            </div>}
                         </Link>
                     </li>
                 );
--- apps/benito-web/src/pages/organizations/index.tsx
+++ apps/benito-web/src/pages/organizations/index.tsx
@@ -1,11 +1,13 @@
 import {Route, Routes} from "react-router-dom";
-import ListEntities from "./ListEntities.tsx";
+import ListEntities from "./ListEntities";
+import RegisterOrEditEntity from "./RegisterOrEditEntity";
 
 
 export default function Organizations() {
     return (
         <Routes>
             <Route path={"/"} element={<ListEntities/>} />
+            <Route path={"/register"} element={<RegisterOrEditEntity/>} />
         </Routes>
     )
 }
