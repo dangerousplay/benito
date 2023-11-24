844bdcc1427fac3ee442b3e386f591f7c6cbfe7d
Dangerousplay <dangerousplay715@gmail.com>
2023-11-24 16:59:32 -0300

    [Added] Mobile address view component

---
+++ apps/benito-mobile/src/components/AddressView.tsx
@@ -0,0 +1,28 @@
+import {SText, SView} from "@/components/core";
+import {CompassIcon, PinIcon} from "benito-common/icons";
+import {formatAddress} from "benito-common/address";
+import {formatDistance} from "benito-common/geolocation";
+import React from "react";
+import {Address} from "benito-common/hooks";
+
+export type AddressViewProps = {
+    address: Address
+    distance?: number
+    className?: string
+}
+
+export const AddressView = ({address, distance, className = ''}: AddressViewProps) => {
+    return (
+        <SView className={`flex-row items-center gap-x-8 ${className}`}>
+            <SView className={"flex-row items-center gap-x-1 w-[60%]"}>
+                <PinIcon />
+                <SText className={"font-light"}>{formatAddress(address)}</SText>
+            </SView>
+
+            {distance && <SView className={"flex-row items-center space-x-1"}>
+                <CompassIcon />
+                <SText className={"font-light"}>{formatDistance(distance)}</SText>
+            </SView>}
+        </SView>
+    )
+}
--- apps/benito-mobile/src/components/index.tsx
+++ apps/benito-mobile/src/components/index.tsx
@@ -2,3 +2,4 @@ export * from './DropdownMenu';
 export * from './Chip';
 export * from './CardSelector';
 
+export * from './AddressView';
