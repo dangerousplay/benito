839418be0f854e646ae2a9ac7be53bec859f2f42
Dangerousplay <dangerousplay715@gmail.com>
2023-11-24 18:43:12 -0300

    [Added] Mobile entity working time component

---
+++ apps/benito-mobile/src/components/WorkingTime.tsx
@@ -0,0 +1,24 @@
+import {ClockIcon} from "benito-common/icons/ClockSvg";
+import {SText, SView} from "@/components/core";
+import React from "react";
+import {formatHour} from "benito-common/address";
+
+
+export type WorkingTimeProps = {
+    opensAt: number
+    closesAt: number
+    workingDays: number[]
+}
+
+export const WorkingTime = ({opensAt, closesAt}: WorkingTimeProps) => {
+    return (
+        <SView className={"flex-row items-center space-x-1"}>
+            <ClockIcon />
+            <SView>
+                <SText className={"font-light"}>
+                    Aberto das {formatHour(opensAt)} às {formatHour(closesAt)}
+                </SText>
+            </SView>
+        </SView>
+    )
+}
--- apps/benito-mobile/src/components/index.tsx
+++ apps/benito-mobile/src/components/index.tsx
@@ -3,3 +3,5 @@ export * from './Chip';
 export * from './CardSelector';
 export * from './TopBar';
 export * from './AddressView';
+export * from './WorkingTime';
+export * from './Button';
