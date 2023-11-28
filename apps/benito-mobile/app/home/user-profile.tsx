06b08e8b4d96ec6c9c7d26c3132f74effd91dfae
Dangerousplay <dangerousplay715@gmail.com>
2023-11-28 17:30:36 -0300

    [Added] Mobile login or register prototype

--- apps/benito-mobile/app/home/user-profile.tsx
+++ apps/benito-mobile/app/home/user-profile.tsx
@@ -1,14 +1,38 @@
-import { Text, View } from 'react-native';
-import { Base } from '@/components/base';
+import {Text, TextInput, View} from 'react-native';
+import {Button} from "@/components";
+
+const LoginOrRegister = () => {
+    return (
+        <View className="flex-1 items-center justify-center space-y-10">
+            <View className="w-2/3 space-y-1">
+                <Text>Email</Text>
+                <View className="rounded-2xl border-2">
+                    <TextInput className="mx-2" />
+                </View>
+            </View>
+
+            <View className="w-2/3 space-y-1">
+                <Text>Senha</Text>
+                <View className="rounded-2xl border-2">
+                    <TextInput className="mx-2" secureTextEntry />
+                </View>
+            </View>
+
+            <View className="w-2/3 pt-10">
+                <Button variant={'primary'}>Login</Button>
+                <Button variant={'secondary'} classesName={"mt-2"}>Registrar-se</Button>
+            </View>
+            <View className="w-2/3">
+
+            </View>
+        </View>
+    )
+};
+
 
 export const UserProfile = () => {
   return (
-    <Base>
-      <View className="items-center">
-        <Text className="text-2xl font-bold">User profile Tab</Text>
-        <Text>A great place to stay :)</Text>
-      </View>
-    </Base>
+      <LoginOrRegister/>
   )
 };
 
